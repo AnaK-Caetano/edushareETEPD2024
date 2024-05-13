670cad1adaa55410485965f66ebf20f97b6b0227
Ana Karina Caetano <103366118+AnaK-Caetano@users.noreply.github.com>
2024-05-13 22:47:57 +0000

    feat: tab navigation

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthStack from './src/navigation/AuthStack';
+import TabRoutes from './src/navigation/BottomTabNavigator';
 
 const Stack = createNativeStackNavigator();
 
