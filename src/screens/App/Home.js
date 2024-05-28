c9f266326861a93dcf5021f9a4e232f7e0610e27
Ana Karina Caetano <103366118+AnaK-Caetano@users.noreply.github.com>
2024-05-28 22:49:19 +0000

    feat: Scrowview

--- src/screens/App/Home.js
+++ src/screens/App/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Card from '../../components/Card/Card';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const Home = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home</Text>
+      <ScrollView>
       <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+
+      </ScrollView>
     </View>
   );
 }
