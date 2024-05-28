745c31a52e2260f06e74413195a45071f567f9e3
Ana Karina Caetano <>
2024-05-27 22:07:01 -0300

    feat: implementando card na tela de projetos

--- src/screens/App/Home.js
+++ src/screens/App/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home</Text>
+      <Card></Card>
     </View>
   );
 }
--- src/screens/App/Projects.js
+++ src/screens/App/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Card from '../../components/Card/Card';
 
 const Projects = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Projects</Text>
+      <Card></Card>
     </View>
   );
 }
